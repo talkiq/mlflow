3a60dd1086fa504ea03552d49e3d65fc6b24b868
Jonathan Johnston <jonathan.david.johnston@gmail.com>
2023-03-24 10:08:04 -0400

    fix(server): avoid autogen run name in create_run

--- mlflow/server/handlers.py
+++ mlflow/server/handlers.py
@@ -675,13 +675,12 @@ def _create_run():
     )
 
     tags = [RunTag(tag.key, tag.value) for tag in request_message.tags]
-    run_name = _generate_random_name() if not request_message.run_name else request_message.run_name
     run = _get_tracking_store().create_run(
         experiment_id=request_message.experiment_id,
         user_id=request_message.user_id,
         start_time=request_message.start_time,
         tags=tags,
-        run_name=run_name,
+        run_name=request_message.run_name,
     )
 
     response_message = CreateRun.Response()
